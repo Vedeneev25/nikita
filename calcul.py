6f0253c89ccbe452118565e08d6700d826b8fbfc
vedeneev <vedeveev@gmail.com>
2021-03-20 09:09:08 +0300

    нет приоритета

--- calcul.py
+++ calcul.py
@@ -1,60 +1,60 @@
-str_command = input("введи команду ")
+#simple div
 
-sF = ''
+str_command = input("Please type command a + b or a-b: ")
+# str_command = input()
+str_command.replace(' ', '')
+
+'''
+parsing
+'''
+
+hp_ops = tuple('^')
+mp_ops = tuple('*/')
+lp_ops = ('+', '-')
+supported_ops = hp_ops + mp_ops + lp_ops
+digits_chars = tuple(map(str, range(10))) + tuple('.,-')
+
 str_A = ''
-
-sS = ''
 str_B = ''
+actions = []
 
-operation = ''
-i=0
+d = dict()
+d['opr'] = 'None'
+d['val'] = ''
+actions.append(d)
 
-while i < len(str_command):
-	print (str_command [i])
-	if str_command[i] == '+' or str_command[i] == '-' or str_command[i] == '*' or str_command[i] == '/' or str_command[i] == '^':
-		if str_A == '':
-			sF = str_command[i]
-		elif operation != '':
-			sS = str_command[i]
-		else:
-			operation = str_command[i]
-	else:	
-		if operation == '':
-			str_A = str_A + str_command[i]
-		else:
-			str_B = str_B + str_command[i]
-	i += 1 
-	pass
+for i, letter in enumerate(str_command):
+	if letter in supported_ops and (i > 0) and actions[-1].get('val') != '':
+		actions.append({'opr': letter,
+						'val': ''})
+	elif letter in digits_chars:
+		actions[-1]['val'] = actions[-1].get('val') + letter
 
-str_A = sF + str_A.strip()
-str_B = sS + str_B.strip()
-print(str_A)
-print(str_B)
-
-
-delimoe = float(str_A)
-
-delitel = float(str_B)
+'''
+calculation
+'''
+#variables = list(map(float, variables))
+for action in actions:
+	action['val'] = float(action.get('val'))
 
 result = None
 
-if operation == '/':
-    if delitel == 0:
-    	result = 'Inf'
-    else:
-    	result = delimoe / delitel
+for action in actions:
+	if type(result) == str:
+		break
 
-elif operation == '+':
-	result= delimoe + delitel
-elif operation == '-':
-	result = delimoe - delitel
-elif operation == '*':
-	result = delimoe * delitel
-elif operation == '^':
-	result = delimoe ** delitel
+	var_A = result
+	var_B = action.get('val')
+	operation = action.get('opr')
 
-else: 
-	result = "unknown"
-
+	if operation in '+-*/':
+		if var_B == 0 and operation == '/':
+			result = 'Inf'
+		else:
+			result = eval('{0}{1}{2}'.format(var_A, operation, var_B))
+	elif operation == '^':
+		result = var_A ** var_B
+	else:
+		result = var_B
 
 print("Result: " + str(result))
